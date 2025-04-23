845e1d533959bc0cd3bf34c84a285148d156915e
Tobias <t.hc199811@gmail.com>
2025-04-23 09:24:58 +0200

    feat: fetch and display app names for selected team

--- src/lib/teamService.ts
+++ src/lib/teamService.ts
@@ -7,6 +7,11 @@ export type Team = {
 	name: string;
 };
 
+export type Application = {
+	id: number;
+	name: string;
+}
+
 export type FetchFn = (input: RequestInfo | URL, init?: RequestInit) => Promise<Response>;
 
 export const teamService = {
@@ -52,6 +57,59 @@ export const teamService = {
 			return {
 				success: true,
 				data: body.teams
+			};
+		} catch (e) {
+			const errorMessage = (e as Error).message ?? 'Unknown error';
+			return {
+				success: false,
+				errorMessage,
+				error: e as Error
+			};
+		}
+	},
+	getApps: async function(
+		teamId: number,
+		sessionId: string,
+		fetchFn: FetchFn = fetch,
+	): Promise<ServiceResponse<Application>> {
+		try {
+			const res = await fetchFn(`${baseUrl}/app/v1/teams/${teamId}/apps`, {
+				headers: {
+					Authorization: `Bearer ${sessionId}`
+				}
+			});
+			if (!res.ok) {
+				let body: string | any;
+				if (res.headers.get('Content-type')?.includes('application/json')) {
+					body = await res.json();
+				} else {
+					body = await res.text();
+				}
+
+				let errorMessage: string;
+				if (typeof body == 'string') {
+					errorMessage = body;
+				} else {
+					errorMessage = body.message ?? 'Unknown error';
+				}
+
+				return {
+					success: false,
+					errorMessage
+				};
+			}
+
+			const body = await res.json();
+			if (body.apps === undefined || !Array.isArray(body.apps)) {
+				return {
+					success: false,
+					errorMessage: 'No or malformed apps returned'
+				};
+			}
+
+			return {
+				success: true,
+				data: body.apps
 			};
 		} catch (e) {
 			const errorMessage = (e as Error).message ?? 'Unknown error';
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,7 +1,37 @@
 <script lang="ts">
+	import { teamService } from "$lib/teamService";
 	import { selectedTeam } from '$lib/state.svelte';
+	import { sessionStore } from "$lib/store";
+	import { get } from 'svelte/store';
+
+	const sessionId = get(sessionStore);
 	const selected = $derived(selectedTeam.selected);
+	const appsPromise = $derived.by(() => {
+		let res 
+		if (selected != undefined) {
+			res = teamService.getApps(selected.id, sessionId)
+		} else {
+			res = new Promise((res) => res([]))
+		}
+
+		return res
+	});
 </script>
 
 <h1>This is the app part</h1>
 <p>{selected?.name ?? 'No team selected'}</p>
+
+{#await appsPromise}
+	<p>loading apps...</p>
+{:then res}
+	{#if res.success}
+		{#if res.data.length == 0 }
+			<p>No applications for this team...</p>
+		{/if}
+		{#each res.data as app}
+			<p>{app.name}</p>
+		{/each}
+	{:else}
+		<p>{res.errorMessage}</p>
+	{/if}
+{/await}
